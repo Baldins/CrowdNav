6d7121f7f691260a992bad01113e2eb4052a78b6
Baldins <fbaldini@caltech.edu>
2021-02-23 12:22:53 -0800

    igp

--- crowd_sim/envs/policy/igp_dist.py
+++ crowd_sim/envs/policy/igp_dist.py
@@ -33,12 +33,12 @@ class Igp_Dist(Policy):
         self.obsv_y = []
         self.obsv_len = 2
         self.count = 0
-        self.vel = 0.2
+        self.vel = 0.5
         self.collision_thresh = 0.5
         self.len_scale = 5
         self.num_agents = 8
-        self.cov_thred_x = 0.02
-        self.cov_thred_y = 0.02
+        self.cov_thred_x = 0.1
+        self.cov_thred_y = 0.1
         self.obsv_err_magnitude = 0.001
         self.a = 0.1  # a controls safety region
         self.h = 10.0  # h controls safety weight
@@ -150,8 +150,8 @@ class Igp_Dist(Policy):
 
 
                 theta = np.arctan2(opt_robot_y - robot_y, opt_robot_x - robot_x)
-                vel_x = 0.5* np.cos(theta) * robot_state.v_pref
-                vel_y = 0.5* np.sin(theta) * robot_state.v_pref
+                vel_x = 0.8* np.cos(theta) * robot_state.v_pref
+                vel_y = 0.8* np.sin(theta) * robot_state.v_pref
             else:
                 # vel_x = 0.00000001 * (opt_robot_x - robot_x) / self.dt
                 # vel_y = 0.00000001 * (opt_robot_y - robot_y) / self.dt
